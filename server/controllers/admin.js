6bbd9a1de4e1e8f416bf09ef60114fa72d34df92
Raj kumar <glrajv369@gmail.com>
2023-05-05 17:15:40 +0530

    admin added for fine tuning

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,10 +1,31 @@
 import express from 'express';
-import { getAllFineTunes } from '../lib/openAi.js';
+import {
+  getAllFineTunes, createFineTune,
+  getAllUploadedFiles, uploadFile, getAllAvailableModels
+} from '../lib/openAi.js';
 
 const router = express.Router()
 
+router.get('/available-modals', async (req, res) => {
+  const data = await getAllAvailableModels()
+  return res.json(data)
+})
+
 router.get('/all-fine-tunes', async (req, res) => {
-  return getAllFineTunes()
+  const data = await getAllFineTunes()
+  return res.json(data)
+})
+
+router.get('/all-uploaded-files', async (req, res) => {
+  const data = await getAllUploadedFiles()
+  return res.json(data)
+})
+
+// for simplicity, added file locally and using with get req
+router.get('/create-new-fine-tune', async (req, res) => {
+  const fileData = await uploadFile("about-me.jsonl")
+  const data = createFineTune(fileData.id)
+  return res.json(data)
 })
 
 export default router
--- server/lib/openAi.js
+++ server/lib/openAi.js
@@ -1,6 +1,11 @@
 import { Configuration, OpenAIApi } from "openai";
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import dotenv from 'dotenv';
 dotenv.config()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const openAi = new OpenAIApi(
   new Configuration({
@@ -16,7 +21,26 @@ export const addMsg = async (role, content) => {
 }
 
 export const getAllFineTunes = async () => {
-  return openAi.listFineTunes()
+  return openAi.listFineTunes().then(r => r.data)
+}
+
+export const getAllUploadedFiles = async () => {
+  return openAi.listFiles().then(r => r.data)
+}
+
+export const getAllAvailableModels = async () => {
+  return openAi.listModels().then(r => r.data)
+}
+
+export const uploadFile = async (fileName) => {
+  return openAi.createFile(
+    createReadStream(__dirname + `/data/${fileName}`),
+    "fine-tune"
+  ).then(r => r.data)
+}
+
+export const createFineTune = async (training_file) => {
+  return openAi.createFineTune({ training_file }).then(r => r.data)
 }
 
 export default openAi
